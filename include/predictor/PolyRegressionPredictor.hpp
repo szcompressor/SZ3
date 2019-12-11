d819600a64031730efe698aff3afd5e6013f907c
kzhao <zhaokaijia@gmail.com>
2019-12-11 11:58:54 -0600

    add poly regression

--- include/predictor/PolyRegressionPredictor.hpp
+++ include/predictor/PolyRegressionPredictor.hpp
@@ -12,12 +12,10 @@
 
 namespace SZ {
 
-#define COEF_N 10
-#define COEF_AUX_DIM 10
-#define COEF_AUX_DIM_START 3
+#define COEF_AUX_MAX_BLOCK 17
 
     // N-d regression predictor
-    template<class T, uint N>
+    template<class T, uint N, uint M = (N + 1) * (N + 2) / 2>
     class PolyRegressionPredictor {
     public:
         static const uint8_t predictor_id = 0b00000011;
@@ -53,23 +51,40 @@ namespace SZ {
             for (int i = 0; i < N; i++) {
                 dims[i] = range->get_dimensions(i);
             }
-            std::array<T, COEF_N> coeffs = compute_regression_coefficients(range, dims);
+            std::array<T, M> coeffs = compute_regression_coefficients(range, dims);
             pred_and_quantize_coefficients(coeffs);
             std::copy(coeffs.begin(), coeffs.end(), current_coeffs.begin());
         }
 
-        inline std::array<T, COEF_N> get_poly_index(const iterator &iter) const {
+        template<uint NN = N>
+        inline typename std::enable_if<NN == 1, std::array<T, M>>::type get_poly_index(const iterator &iter) const {
+            T i = iter.get_current_index(0);
+
+            return std::array<T, M>{1.0, i, i * i};
+        }
+
+        template<uint NN = N>
+        inline typename std::enable_if<NN == 2, std::array<T, M>>::type get_poly_index(const iterator &iter) const {
+            T i = iter.get_current_index(0);
+            T j = iter.get_current_index(1);
+
+            return std::array<T, M>{1.0, i, j, i * i, i * j, j * j};
+        }
+
+        template<uint NN = N>
+        inline typename std::enable_if<NN != 1 && NN != 2, std::array<T, M>>::type
+        get_poly_index(const iterator &iter) const {
             T i = iter.get_current_index(0);
             T j = iter.get_current_index(1);
             T k = iter.get_current_index(2);
 
-            return std::array<T, COEF_N>{1.0, i, j, k, i * i, i * j, i * k, j * j, j * k, k * k};
+            return std::array<T, M>{1.0, i, j, k, i * i, i * j, i * k, j * j, j * k, k * k};
         }
 
         inline T predict(const iterator &iter) const noexcept {
             T pred = 0;
-            auto poly_index = get_poly_index(iter);
-            for (int i = 0; i < COEF_N; i++) {
+            auto poly_index = get_poly_index<N>(iter);
+            for (int i = 0; i < M; i++) {
                 pred += poly_index[i] * current_coeffs[i];
             }
             return pred;
@@ -115,63 +130,53 @@ namespace SZ {
             std::cout << "Regression predictor, eb = " << quantizer.get_eb() << "\n";
         }
 
-        void display_coef_aux(std::array<T, COEF_N * COEF_N> aux) {
-            for (int i = 0; i < COEF_N; i++) {
-                for (int j = 0; j < COEF_N; j++)
-                    std::cout << std::setw(10) << std::setprecision(6) << aux[i * COEF_N + j] << " ";
-                std::cout << std::endl;
-            }
-        }
-
-        void init_poly() {
-            size_t num = 0;
-            std::string bin_file_path = SZ::get_cmake_project_source_dir() + "/include/predictor/PolyRegressionCoefAux.f32";
-            auto data = SZ::readfile<float>(bin_file_path.data(), num);
-
-            auto data1 = &data[0];
-            coef_aux = std::vector<std::array<T, COEF_N * COEF_N>>((COEF_AUX_DIM + 1) * (COEF_AUX_DIM + 1) * (COEF_AUX_DIM + 1),
-                                                                   {0});
-            for (int i = COEF_AUX_DIM_START; i <= COEF_AUX_DIM; i++) {
-                for (int j = COEF_AUX_DIM_START; j <= COEF_AUX_DIM; j++) {
-                    for (int k = COEF_AUX_DIM_START; k <= COEF_AUX_DIM; k++) {
-                        std::copy_n(data1, COEF_N * COEF_N,
-                                    coef_aux[i * COEF_AUX_DIM * COEF_AUX_DIM + j * COEF_AUX_DIM + k].begin());
-                        data1 += COEF_N * COEF_N;
-                    }
-                }
-            }
-//            display_coef_aux(coef_aux[6 * COEF_AUX_DIM * COEF_AUX_DIM + 6 * COEF_AUX_DIM + 6]);
-        }
 
     private:
         LinearQuantizer<T> quantizer;
         std::vector<int> regression_coeff_quant_inds;
         size_t regression_coeff_index = 0;
-        std::array<T, COEF_N> current_coeffs;
-        std::vector<std::array<T, COEF_N * COEF_N>> coef_aux;
+        std::array<T, M> current_coeffs;
+        std::vector<std::array<T, M * M>> coef_aux_list;
 
-        std::array<T, COEF_N>
+        void init_poly() {
+            size_t num = 0;
+            std::string bin_file_path =
+                    SZ::get_cmake_project_source_dir() + "/data/PolyRegressionCoefAux" + std::to_string(N) + "D.f32";
+//            std::cout << bin_file_path << std::endl;
+            auto data = SZ::readfile<float>(bin_file_path.data(), num);
+
+            auto coef_aux_p = &data[0];
+            coef_aux_list = std::vector<std::array<T, M * M>>(
+                    (COEF_AUX_MAX_BLOCK + 1) * (COEF_AUX_MAX_BLOCK + 1) * (COEF_AUX_MAX_BLOCK + 1), {0});
+            while (coef_aux_p < &data[0] + num) {
+                std::array<size_t, N> dims;
+                for (auto &idx:dims) {
+                    idx = *coef_aux_p++;
+                }
+                std::copy_n(coef_aux_p, M * M, coef_aux_list[get_coef_aux_list_idx(dims)].begin());
+                coef_aux_p += M * M;
+            }
+//            display_coef_aux(coef_aux_list[get_coef_aux_list_idx( std::array<size_t, N>{6, 6, 6})]);
+        }
+
+        std::array<T, M>
         compute_regression_coefficients(const std::shared_ptr<Range> &range, const std::array<size_t, N> &dims) const {
-            std::array<double, COEF_N> sum{0};
+            std::array<double, M> sum{0};
             {
                 for (auto iter = range->begin(); iter != range->end(); ++iter) {
                     T data = *iter;
-                    auto poly_index = get_poly_index(iter);
-                    for (int i = 0; i < COEF_N; i++) {
+                    auto poly_index = get_poly_index<N>(iter);
+                    for (int i = 0; i < M; i++) {
                         sum[i] += poly_index[i] * data;
                     }
                 }
             }
-            std::array<double, COEF_N> coeffs{0};
-            auto coef_aux_index = 0;
-            for (auto &dim:dims) {
-                coef_aux_index = coef_aux_index * COEF_AUX_DIM + dim;
-            }
-            auto coef_aux1 = coef_aux[coef_aux_index];
+            std::array<double, M> coeffs{0};
+            auto coef_aux_idx = coef_aux_list[get_coef_aux_list_idx(dims)];
 
-            for (int i = 0; i < COEF_N; i++) {
-                for (int j = 0; j < COEF_N; j++) {
-                    coeffs[i] += coef_aux1[i * COEF_N + j] * sum[j];
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < M; j++) {
+                    coeffs[i] += coef_aux_idx[i * M + j] * sum[j];
                 }
             }
 //            std::cout << "sum ";
@@ -186,14 +191,14 @@ namespace SZ {
 //            }
 //            std::cout << std::endl;
 
-            std::array<T, COEF_N> coeffsT;
-            for (int i = 0; i < COEF_N; i++) {
+            std::array<T, M> coeffsT;
+            for (int i = 0; i < M; i++) {
                 coeffsT[i] = coeffs[i];
             }
             return coeffsT;
         }
 
-        void pred_and_quantize_coefficients(std::array<T, COEF_N> &coeffs) {
+        void pred_and_quantize_coefficients(std::array<T, M> &coeffs) {
             for (int i = 0; i <= N; i++) {
                 regression_coeff_quant_inds.push_back(quantizer.quantize_and_overwrite(coeffs[i], current_coeffs[i]));
             }
@@ -204,8 +209,23 @@ namespace SZ {
                 coeffs = quantizer.recover(coeffs, regression_coeff_quant_inds[regression_coeff_index++]);
             }
         }
-    };
 
+        void display_coef_aux(std::array<T, M * M> aux) {
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < M; j++)
+                    std::cout << std::setw(10) << std::setprecision(6) << aux[i * M + j] << " ";
+                std::cout << std::endl;
+            }
+        }
+
+        inline size_t get_coef_aux_list_idx(const std::array<size_t, N> &dims) const {
+            auto coef_aux_index = 0;
+            for (auto &dim:dims) {
+                coef_aux_index = coef_aux_index * COEF_AUX_MAX_BLOCK + dim;
+            }
+            return coef_aux_index;
+        }
+    };
 
 }
 
