ea8cf4b76e1301de57e74e356c56e0f6e0504293
kzhao <zhaokaijia@gmail.com>
2020-06-15 12:31:20 -0400

    add lz4 lossless compressor

---
+++ include/lossless/Lossless_lz4.hpp
@@ -0,0 +1,49 @@
+//
+// Created by Kai Zhao on 4/21/20.
+//
+
+#ifndef SZ_LOSSLESS_LZ4_HPP
+#define SZ_LOSSLESS_LZ4_HPP
+
+#include "lz4/lz4.h"
+#include "def.hpp"
+#include "utils/MemoryOps.hpp"
+#include "utils/fileUtil.h"
+#include "Lossless.hpp"
+
+namespace SZ {
+    class Lossless_LZ4 : public concepts::LosslessInterface {
+
+    public:
+        uchar *compress(uchar *data, size_t dataLength, size_t &outSize) {
+            size_t estimatedCompressedSize = dataLength < 100 ? 200 : dataLength * 1.2;
+            uchar *compressBytes = new uchar[estimatedCompressedSize];
+            uchar *compressBytesPos = compressBytes;
+            write(dataLength, compressBytesPos);
+
+            LZ4_compress_default(data, compressBytesPos, dataLength, estimatedCompressedSize);
+            outSize += sizeof(size_t);
+            return compressBytes;
+        }
+
+        void postcompress_data(uchar *data) {
+            delete[] data;
+        }
+
+        uchar *decompress(const uchar *data, size_t compressedSize) {
+            const uchar *dataPos = data;
+            size_t dataLength = 0, tmp = compressedSize;
+            read(dataLength, dataPos, tmp);
+
+            uchar *oriData = new uchar[dataLength];
+            LZ4_decompress_safe(dataPos, oriData, compressedSize, dataLength);
+            return oriData;
+        }
+
+        void postdecompress_data(uchar *data) {
+            delete[] data;
+        }
+
+    };
+}
+#endif //SZ_LOSSLESS_LZ4_HPP
