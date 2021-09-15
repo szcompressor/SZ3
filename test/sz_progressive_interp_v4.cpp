b6e8bc5afc89433a3e4e170f119b7d6addc29cdd
Kai Zhao <zhaokaijia@gmail.com>
2021-09-15 18:17:39 -0400

    add tuning

--- test/sz_progressive_interp_v4.cpp
+++ test/sz_progressive_interp_v4.cpp
@@ -1,4 +1,5 @@
 #include <compressor/SZProgressiveInterpolationCompressorV4.hpp>
+#include <compressor/SZBlockInterpolationCompressor.hpp>
 #include <compressor/SZInterpolationCompressor.hpp>
 #include <quantizer/IntegerQuantizer.hpp>
 #include <predictor/ComposedPredictor.hpp>
@@ -92,6 +93,140 @@ void interp_compress_decompress(const char *path, double eb, int interp_op, int 
 
 }
 
+template<uint N>
+double interp_compress_test_block(float *data, size_t num, double eb, int interp_level, int interp_op, int direction_op,
+                                  int block_size, int interp_block_size, std::array<size_t, N> dims) {
+
+    std::cout << "****************** compression ****************" << std::endl;
+    std::cout << "Interp Level       = " << interp_level << std::endl
+              << "Interp Op          = " << interp_op << std::endl
+              << "Direction          = " << direction_op << std::endl
+              << "SZ block size      = " << block_size << std::endl
+              << "Interp block size  = " << interp_block_size << std::endl;
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    std::vector<float> data1(data, data + num);
+    size_t compressed_size = 0;
+
+    SZ::Config<float, N> conf(eb, dims);
+    conf.block_size = block_size;
+    conf.stride = conf.block_size;
+    auto sz = SZ::make_sz_fast_block_interpolation_compressor(
+            conf,
+            SZ::SimplePredictor<float, N>(eb),
+            SZ::LinearQuantizer<float>(eb),
+            SZ::HuffmanEncoder<int>(),
+            SZ::Lossless_zstd(),
+            interp_op,
+            direction_op,
+            interp_level
+    );
+
+    sz.compress(data1.data(), compressed_size);
+    clock_gettime(CLOCK_REALTIME, &end);
+    double compression_time =
+            (double) (end.tv_sec - start.tv_sec) + (double) (end.tv_nsec - start.tv_nsec) / (double) 1000000000;
+    auto compression_ratio = num * sizeof(float) * 1.0 / compressed_size;
+    std::cout << "Compressed size = " << compressed_size << std::endl;
+    std::cout << "Compression ratio = " << compression_ratio << std::endl;
+    std::cout << "Tuning Interp Compression time = " << compression_time
+              << " Ratio = " << compression_ratio
+              << " Params = " << interp_level
+              << " " << interp_op
+              << " " << direction_op
+              << " " << block_size
+              << " " << interp_block_size
+              << std::endl;
+
+    std::cout << "****************** end ****************" << std::endl;
+    return compression_ratio;
+}
+
+template<uint N>
+float cal_sampling_ratio(size_t block, size_t n, size_t dmin, std::array<size_t, N> dims) {
+    size_t sample_n = 1;
+    for (auto dim: dims) {
+        sample_n *= dim / dmin * 2 * block;
+    }
+    return sample_n * 1.0 / n;
+}
+
+template<uint N>
+std::pair<int, int> interp_tuning(char *path, double eb, std::array<size_t, N> dims) {
+    std::cout << "================================ BEGIN TUNING ================================" << std::endl;
+
+
+    size_t num = 0;
+    auto data = SZ::readfile<float>(path, num);
+    std::cout << "Read " << num << " elements\n";
+
+    size_t dmin = *std::min_element(dims.begin(), dims.end());
+    int sampling_block = dmin;
+    while (cal_sampling_ratio<3>(sampling_block, num, dmin, dims) > 0.035) {
+        sampling_block--;
+    }
+    if (sampling_block * 2 > dmin) {
+        sampling_block = dmin / 2;
+    }
+    size_t b1 = dims[0] / dmin;
+    size_t b2 = dims[1] / dmin;
+    size_t b3 = dims[2] / dmin;
+    size_t r1 = b1 * 2 * sampling_block;
+    size_t r2 = b2 * 2 * sampling_block;
+    size_t r3 = b3 * 2 * sampling_block;
+    size_t di, dj, dk;
+    size_t sampling_num = r1 * r2 * r3;
+    std::array<size_t, 3> sampling_dims{r1, r1, r3};
+    std::vector<float> sampling_data(sampling_num, 0);
+    for (size_t bi = 0; bi < b1; bi++) {
+        for (size_t bj = 0; bj < b2; bj++) {
+            for (size_t bk = 0; bk < b3; bk++) {
+                for (size_t i = 0; i < 2 * sampling_block; i++) {
+                    for (size_t j = 0; j < 2 * sampling_block; j++) {
+                        for (size_t k = 0; k < 2 * sampling_block; k++) {
+                            di = i < sampling_block ? i + sampling_block : dmin - 3 * sampling_block + i;
+                            dj = j < sampling_block ? j + sampling_block : dmin - 3 * sampling_block + j;
+                            dk = k < sampling_block ? k + sampling_block : dmin - 3 * sampling_block + k;
+                            auto d = data[(bi * dmin + di) * dims[1] * dims[2] + (bj * dmin + dj) * dims[2] +
+                                          bk * dmin + dk];
+                            sampling_data[(bi * 2 * sampling_block + i) * r2 * r3
+                                          + (bj * 2 * sampling_block + j) * r3
+                                          + bk * 2 * sampling_block + k] = d;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    printf("Tuning sampling block = %d percent = %.3f%% Time = %.3f \n",
+           sampling_block, sampling_num * 100.0 / num, 0.0);
+
+    int interp_level = -1, interp_op, direction_op = 0, block_size = sampling_block, interp_block_size = sampling_block;
+
+    double best_interp_ratio = 0, ratio;
+    for (int i = 0; i < 2; i++) {
+        ratio = interp_compress_test_block<N>(sampling_data.data(), sampling_num, eb, interp_level, i, direction_op,
+                                              block_size, interp_block_size, sampling_dims);
+        if (ratio > best_interp_ratio) {
+            best_interp_ratio = ratio;
+            interp_op = i;
+        }
+    }
+    std::cout << "Tuning interp best interp_op = " << interp_op << " , best ratio = " << best_interp_ratio << std::endl;
+
+    ratio = interp_compress_test_block<N>(sampling_data.data(), sampling_num, eb, interp_level, interp_op, 5,
+                                          block_size, interp_block_size, sampling_dims);
+    if (ratio > best_interp_ratio * 1.02) {
+        direction_op = 5;
+    }
+    std::cout << "Tuning interp interp_op = " << interp_op << ", direction_op = " << direction_op << std::endl;
+    std::cout << "================================ END TUNING ================================" << std::endl;
+
+    return std::pair<int, int>{interp_op, direction_op};
+
+}
 
 int main(int argc, char **argv) {
 
@@ -112,6 +247,16 @@ int main(int argc, char **argv) {
     }
     if (argp < argc) {
         direction_op = atoi(argv[argp++]);
+    }
+    if (interp_op == -1 || direction_op == -1) {
+        if (dim != 3) {
+            std::cout << "Tuning only support 3D array.\n";
+            return 0;
+        }
+        std::array<size_t, 3> dims_array{dims[0], dims[1], dims[2]};
+        auto x = interp_tuning<3>(argv[1], eb, dims_array);
+        interp_op = x.first;
+        direction_op = x.second;
     }
 
     int level_fill = 0, level_independent = 0;
