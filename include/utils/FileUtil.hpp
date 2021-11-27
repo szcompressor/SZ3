9d491eea867e85a13771ac37a8928dedbbb833d8
Kai Zhao <zhaokaijia@gmail.com>
2021-11-27 11:40:11 -0500

    add polyregression predictor

--- include/utils/FileUtil.hpp
+++ include/utils/FileUtil.hpp
@@ -60,7 +60,7 @@ namespace SZ {
     std::unique_ptr<Type[]> readfile(const char *file, size_t &num) {
         std::ifstream fin(file, std::ios::binary);
         if (!fin) {
-            std::cout << " Error, Couldn't find the file" << "\n";
+            std::cout << " Error, Couldn't find the file" << file << "\n";
             return 0;
         }
         fin.seekg(0, std::ios::end);
