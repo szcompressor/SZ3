2d004b24f0cb01e76f81a55ff26a9640130492f3
Kai Zhao <zhaokaijia@gmail.com>
2022-02-02 20:13:40 -0500

    bugfix

--- include/SZ3/utils/QuantOptimizatioin.hpp
+++ include/SZ3/utils/QuantOptimizatioin.hpp
@@ -2,6 +2,7 @@
 #define SZ_optimize_quant_intervals_hpp
 
 #include <vector>
+#include "SZ3/predictor/MetaLorenzoPredictor.hpp"
 
 namespace SZ {
 
@@ -105,7 +106,7 @@ namespace SZ {
         float pred_err = 0;
         int radius = (QuantIntvMeanCapacity >> 1);
         while (data_pos - data < len) {
-            pred_value = lorenzo_predict_3d(data_pos, r23, r3);
+            pred_value = SZMETA::lorenzo_predict_3d(data_pos, r23, r3);
             pred_err = fabs(pred_value - *data_pos);
             if (pred_err < precision) freq_count++;
             pred_index = (pred_err / precision + 1) / 2;
