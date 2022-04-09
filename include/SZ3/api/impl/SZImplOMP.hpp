a5b4659b8b9db3d23e177d9740da7ff5fb683290
Kai Zhao <zhaokaijia@gmail.com>
2022-04-09 16:59:30 -0400

    bugfix for omp

--- include/SZ3/api/impl/SZImplOMP.hpp
+++ include/SZ3/api/impl/SZImplOMP.hpp
@@ -10,6 +10,8 @@
 
 template<class T, SZ::uint N>
 char *SZ_compress_OMP(SZ::Config &conf, T *data, size_t &outSize) {
+    unsigned char *compressed, *compressed_pos;
+
 #ifdef _OPENMP
 
     assert(N == conf.N);
@@ -18,7 +20,6 @@ char *SZ_compress_OMP(SZ::Config &conf, T *data, size_t &outSize) {
         exit(0);
     }
 
-    unsigned char *compressed, *compressed_pos;
     std::vector<char *> compressed_t;
     std::vector<size_t> cmp_size_t, cmp_start_t;
     std::vector<T> min_t, max_t;
@@ -95,7 +96,7 @@ char *SZ_compress_OMP(SZ::Config &conf, T *data, size_t &outSize) {
 
     outSize = compressed_pos - compressed + cmp_start_t[nThreads];
 #endif
-    return (char *) nullptr;
+    return (char *) compressed;
 }
 
 
