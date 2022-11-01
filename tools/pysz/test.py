c5b806749b7e0a38cb5055b236514641e57042c0
Kai Zhao <zhaokaijia@gmail.com>
2022-11-01 11:30:28 -0500

    python api for sz2/sz3

--- tools/pysz/test.py
+++ tools/pysz/test.py
@@ -3,16 +3,18 @@ from pathlib import Path
 from pysz import SZ
 import platform
 
+
 # prepare your data
 HOME = str(Path.home())
 data = np.fromfile(HOME + '/data/hurricane-100x500x500/Uf48.bin.dat', dtype=np.float32)
 data = np.reshape(data, (100, 500, 500))
 
+
+# init SZ with the c dynamic library. Please change the path to your so/dylib file
+# Both SZ2 and SZ3 are supported
 lib_extention = "so" if platform.system() == 'Linux' else "dylib"
-# init SZ with the c dynamic library. Please change the path based on your system
-sz3 = SZ("../../build/tools/sz3c/libSZ3c.{}".format(lib_extention))
-# sz2 = SZ("../../../sz2/build/sz/libSZ.{}".format(lib_extention))
-sz = sz3
+sz = SZ("../../build/tools/sz3c/libSZ3c.{}".format(lib_extention))
+# sz = SZ("../../../sz2/build/sz/libSZ.{}".format(lib_extention))
 
 # compress, both input and output data are numpy array
 data_cmpr, cmpr_ratio = sz.compress(data, 0, 1e-3, 0, 0)
