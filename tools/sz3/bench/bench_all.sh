3cba602e909a0495566d92a9792482d27966152c
Kai Zhao <zhaokaijia@gmail.com>
2023-02-01 20:06:53 -0600

    update perf

--- tools/sz3/bench/bench_all.sh
+++ tools/sz3/bench/bench_all.sh
@@ -4,9 +4,9 @@ exefolder=$1
 
 ./bench.sh $exefolder ~/data/nyx-512x512x512/temperature.dat 3 512 512 512
 ./bench.sh $exefolder ~/data/nyx-512x512x512/velocity_x.dat 3 512 512 512
-./bench.sh $exefolder ~/data/miranda-256x384x384 velocityx.f32.dat  3 256 384 384
-./bench.sh $exefolder ~/data/hurricane-100x500x500 Uf48.bin.dat 3 100 500 500
-./bench.sh $exefolder ~/data/scale-98x1200x1200 QV-98x1200x1200.dat 3 98 1200 1200
-./bench.sh $exefolder ~/data/cesm-26x1800x3600 V_1_26_1800_3600.dat 3 26 1800 3600
-./bench.sh $exefolder ~/data/qmcpack-288x115x69x69 einspline_288_115_69_69.pre.f32.dat 3 33120 69 69
+./bench.sh $exefolder ~/data/miranda-256x384x384/velocityx.f32.dat  3 256 384 384
+./bench.sh $exefolder ~/data/hurricane-100x500x500/Uf48.bin.dat 3 100 500 500
+./bench.sh $exefolder ~/data/scale-98x1200x1200/QV-98x1200x1200.dat 3 98 1200 1200
+./bench.sh $exefolder ~/data/cesm-26x1800x3600/V_1_26_1800_3600.dat 3 26 1800 3600
+./bench.sh $exefolder ~/data/qmcpack-288x115x69x69/einspline_288_115_69_69.pre.f32.dat 3 33120 69 69
 #
