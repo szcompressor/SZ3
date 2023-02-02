f654d9150a38a6ab7f47528dfe62ead0faeb8988
Kai Zhao <zhaokaijia@gmail.com>
2023-02-01 20:03:48 -0600

    update perf

--- tools/sz3/bench/bench.sh
+++ tools/sz3/bench/bench.sh
@@ -8,28 +8,28 @@ r2=$5
 r3=$6
 
 echo $dataset $dim $r1 $r2 $r3
-$exefolder/bench_iterator_sz2 $dataset 3 $dim $r1 $r2 $r3
-$exefolder/bench_iterator_sz2 $dataset 3 $dim $r1 $r2 $r3
+$exefolder/bench_iterator_sz2 $dataset $dim $r1 $r2 $r3
+$exefolder/bench_iterator_sz2 $dataset $dim $r1 $r2 $r3
 
-$exefolder/bench_iterator_sz3 $dataset 3 $dim $r1 $r2 $r3
-$exefolder/bench_iterator_sz3 $dataset 3 $dim $r1 $r2 $r3
+$exefolder/bench_iterator_sz3 $dataset $dim $r1 $r2 $r3
+$exefolder/bench_iterator_sz3 $dataset $dim $r1 $r2 $r3
 
-$exefolder/bench_iterator_hybrid1 $dataset 3 $dim $r1 $r2 $r3
-$exefolder/bench_iterator_hybrid1 $dataset 3 $dim $r1 $r2 $r3
+$exefolder/bench_iterator_hybrid1 $dataset $dim $r1 $r2 $r3
+$exefolder/bench_iterator_hybrid1 $dataset $dim $r1 $r2 $r3
 
-$exefolder/bench_iterator_hybrid2 $dataset 3 $dim $r1 $r2 $r3
-$exefolder/bench_iterator_hybrid2 $dataset 3 $dim $r1 $r2 $r3
+$exefolder/bench_iterator_hybrid2 $dataset $dim $r1 $r2 $r3
+$exefolder/bench_iterator_hybrid2 $dataset $dim $r1 $r2 $r3
 
-$exefolder/bench_iterator_hybrid3 $dataset 3 $dim $r1 $r2 $r3
-$exefolder/bench_iterator_hybrid3 $dataset 3 $dim $r1 $r2 $r3
+$exefolder/bench_iterator_hybrid3 $dataset $dim $r1 $r2 $r3
+$exefolder/bench_iterator_hybrid3 $dataset $dim $r1 $r2 $r3
 
-$exefolder/bench_iterator_hybrid4 $dataset 3 $dim $r1 $r2 $r3
-$exefolder/bench_iterator_hybrid4 $dataset 3 $dim $r1 $r2 $r3
+$exefolder/bench_iterator_hybrid4 $dataset $dim $r1 $r2 $r3
+$exefolder/bench_iterator_hybrid4 $dataset $dim $r1 $r2 $r3
 
-$exefolder/bench_iterator_hybrid5 $dataset 3 $dim $r1 $r2 $r3
-$exefolder/bench_iterator_hybrid5 $dataset 3 $dim $r1 $r2 $r3
+$exefolder/bench_iterator_hybrid5 $dataset $dim $r1 $r2 $r3
+$exefolder/bench_iterator_hybrid5 $dataset $dim $r1 $r2 $r3
 
-$exefolder/bench_iterator_hybrid6 $dataset 3 $dim $r1 $r2 $r3
-$exefolder/bench_iterator_hybrid6 $dataset 3 $dim $r1 $r2 $r3
+$exefolder/bench_iterator_hybrid6 $dataset $dim $r1 $r2 $r3
+$exefolder/bench_iterator_hybrid6 $dataset $dim $r1 $r2 $r3
 
 echo ''
