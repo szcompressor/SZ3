c1d6504e1d0cfa6cbdb718d3a7b396920d3986f9
Kai Zhao <zhaokaijia@gmail.com>
2024-08-14 16:18:29 -0400

    update hdf5 filter

--- include/SZ3/utils/Config.hpp
+++ include/SZ3/utils/Config.hpp
@@ -14,6 +14,17 @@
 #include "SZ3/utils/MemoryUtil.hpp"
 #include "SZ3/utils/inih/INIReader.h"
 
+#define SZ_FLOAT 0
+#define SZ_DOUBLE 1
+#define SZ_UINT8 2
+#define SZ_INT8 3
+#define SZ_UINT16 4
+#define SZ_INT16 5
+#define SZ_UINT32 6
+#define SZ_INT32 7
+#define SZ_UINT64 8
+#define SZ_INT64 9
+
 namespace SZ3 {
 
     enum EB {
@@ -51,21 +62,28 @@ namespace SZ3 {
     class Config {
     public:
         template<class ... Dims>
-        Config(Dims ... args) {
+        Config(Dims... args) {
             dims = std::vector<size_t>{static_cast<size_t>(std::forward<Dims>(args))...};
-            N = dims.size();
-            num = std::accumulate(dims.begin(), dims.end(), (size_t) 1, std::multiplies<size_t>());
-            blockSize = (N == 1 ? 128 : (N == 2 ? 16 : 6));
-            pred_dim = N;
-            stride = blockSize;
+            setDims(dims.begin(), dims.end());
         }
-
+        
         template<class Iter>
         size_t setDims(Iter begin, Iter end) {
-            dims = std::vector<size_t>(begin, end);
+            auto dims_ = std::vector<size_t>(begin, end);
+            dims.clear();
+            for (auto dim : dims_) {
+                if (dim > 1) {
+                    dims.push_back(dim);
+                }
+            }
+            if (dims.empty()) {
+                dims = {1};
+            }
             N = dims.size();
             num = std::accumulate(dims.begin(), dims.end(), (size_t) 1, std::multiplies<size_t>());
             pred_dim = N;
+            blockSize = (N == 1 ? 128 : (N == 2 ? 16 : 6));
+            stride = blockSize;
             return num;
         }
 
@@ -150,6 +168,7 @@ namespace SZ3 {
             write(stride, c);
             write(pred_dim, c);
             write(openmp, c);
+            write(dataType, c);
         };
 
         void load(const unsigned char *&c) {
@@ -176,23 +195,53 @@ namespace SZ3 {
             read(stride, c);
             read(pred_dim, c);
             read(openmp, c);
+            read(dataType, c);
         }
 
         void print() {
+            printf("===================== Begin SZ3 Configuration =====================\n");
+            printf("N = %d\n", N);
+            printf("dims = ");
+            for (auto dim : dims) {
+                printf("%zu ", dim);
+            }
+            printf("\nnum = %zu\n", num);
             printf("CmprAlgo = %s\n", enum2Str((ALGO) cmprAlgo));
+            printf("ErrorBoundMode = %s\n", enum2Str((EB) errorBoundMode));
+            printf("AbsErrorBound = %f\n", absErrorBound);
+            printf("RelErrorBound = %f\n", relErrorBound);
+            printf("PSNRErrorBound = %f\n", psnrErrorBound);
+            printf("L2NormErrorBound = %f\n", l2normErrorBound);
+            printf("Lorenzo = %d\n", lorenzo);
+            printf("Lorenzo2ndOrder = %d\n", lorenzo2);
+            printf("Regression = %d\n", regression);
+            printf("Regression2ndOrder = %d\n", regression2);
+            printf("OpenMP = %d\n", openmp);
+            printf("DataType = %d\n", dataType);
+            printf("Lossless = %d\n", lossless);
+            printf("Encoder = %d\n", encoder);
+            printf("InterpolationAlgo = %s\n", enum2Str((INTERP_ALGO) interpAlgo));
+            printf("InterpolationDirection = %d\n", interpDirection);
+            printf("InterpolationBlockSize = %d\n", interpBlockSize);
+            printf("QuantizationBinTotal = %d\n", quantbinCnt);
+            printf("BlockSize = %d\n", blockSize);
+            printf("Stride = %d\n", stride);
+            printf("PredDim = %d\n", pred_dim);
+            printf("===================== End SZ3 Configuration =====================\n");
         }
-
+        
         static size_t size_est() {
-            return sizeof(size_t) * 5 + sizeof(double) * 4 + sizeof(bool) * 5 + sizeof(uint8_t) * 9 + sizeof(int) * 5 + 50; //50 is for redundancy
+            return sizeof(Config) + sizeof(size_t) * 5 + 32; //sizeof(size_t) * 5 is for dims vector, 32 is for redundancy
         }
-
+        
+        
         std::string sz3DataVer = SZ3_DATA_VER;
         char N = 0;
         std::vector<size_t> dims;
         size_t num = 0;
         uint8_t cmprAlgo = ALGO_INTERP_LORENZO;
         uint8_t errorBoundMode = EB_ABS;
-        double absErrorBound = 0.0;
+        double absErrorBound = 1e-3;
         double relErrorBound = 0.0;
         double psnrErrorBound = 0.0;
         double l2normErrorBound = 0.0;
@@ -201,6 +250,7 @@ namespace SZ3 {
         bool regression = true;
         bool regression2 = false;
         bool openmp = false;
+        uint8_t dataType = SZ_FLOAT; // dataType is only used in HDF5 filter
         uint8_t lossless = 1; // 0-> skip lossless(use lossless_bypass); 1-> zstd
         uint8_t encoder = 1;// 0-> skip encoder; 1->HuffmanEncoder; 2->ArithmeticEncoder
         uint8_t interpAlgo = INTERP_ALGO_CUBIC;
