07cee4b95feaead2e441c45cf241b21ae9feef68
Kai Zhao <zhaokaijia@gmail.com>
2024-12-10 10:32:57 -0500

    update logic for buffer size checking

--- include/SZ3/def.hpp
+++ include/SZ3/def.hpp
@@ -6,7 +6,7 @@ namespace SZ3 {
 typedef unsigned int uint;
 typedef unsigned char uchar;
 #define SZ_ERROR_COMP_BUFFER_NOT_LARGE_ENOUGH \
-    "The buffer for compressed data is not large enough. Ideally, set it at least 2X original data size."
+    "The buffer for compressed data is not large enough."
 }  // namespace SZ3
 
 #endif
