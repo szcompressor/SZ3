44af05076d299b0fc5353d59e578c83c216f4e38
Jliu-1 <jliu217@central.uh.edu>
2025-03-25 10:44:56 -0500

    init

--- include/SZ3/api/impl/SZAlgoInterp.hpp
+++ include/SZ3/api/impl/SZAlgoInterp.hpp
@@ -17,6 +17,10 @@ size_t SZ_compress_Interp(Config &conf, T *data, uchar *cmpData, size_t cmpCap) 
     assert(N == conf.N);
     assert(conf.cmprAlgo == ALGO_INTERP);
     calAbsErrorBound(conf, data);
+    if (conf.maxStep<=0){
+        std::array<size_t,4> anchor_strides={256,64,32,16};
+        conf.maxStep = anchor_strides[N-1];
+    }
 
     auto sz = make_compressor_sz_generic<T, N>(
         make_decomposition_interpolation<T, N>(conf, LinearQuantizer<T>(conf.absErrorBound, conf.quantbinCnt / 2)),
@@ -61,8 +65,14 @@ size_t SZ_compress_Interp_lorenzo(Config &conf, T *data, uchar *cmpData, size_t 
     assert(conf.cmprAlgo == ALGO_INTERP_LORENZO);
 
     //        Timer timer(true);
-
+    std::cout<<"p1"<<std::endl;
     calAbsErrorBound(conf, data);
+
+    if (conf.maxStep<=0){
+        std::array<size_t,4> anchor_strides={256,64,32,16};
+        conf.maxStep = anchor_strides[N-1];
+    }
+
 
     size_t sampling_num, sampling_block;
     std::vector<size_t> sample_dims(N);
@@ -71,7 +81,7 @@ size_t SZ_compress_Interp_lorenzo(Config &conf, T *data, uchar *cmpData, size_t 
         conf.cmprAlgo = ALGO_INTERP;
         return SZ_compress_Interp<T, N>(conf, data, cmpData, cmpCap);
     }
-
+    std::cout<<"p2"<<std::endl;
     double best_lorenzo_ratio = 0, best_interp_ratio = 0, ratio;
     size_t bufferCap = conf.num * sizeof(T);
     auto buffer = static_cast<uchar *>(malloc(bufferCap));
@@ -93,7 +103,7 @@ size_t SZ_compress_Interp_lorenzo(Config &conf, T *data, uchar *cmpData, size_t 
         //    printf("Lorenzo ratio = %.2f\n", ratio);
         best_lorenzo_ratio = sampling_num * 1.0 * sizeof(T) / sampleOutSize;
     }
-
+    std::cout<<"p3"<<std::endl;
     {
         // tune interp
         for (auto &interp_op : {INTERP_ALGO_LINEAR, INTERP_ALGO_CUBIC}) {
@@ -115,12 +125,15 @@ size_t SZ_compress_Interp_lorenzo(Config &conf, T *data, uchar *cmpData, size_t 
             conf.interpDirection = direction_op;
         }
     }
+    std::cout<<"p4"<<std::endl;
 
     bool useInterp = !(best_lorenzo_ratio > best_interp_ratio && best_lorenzo_ratio < 80 && best_interp_ratio < 80);
     size_t cmpSize = 0;
     if (useInterp) {
+
         conf.cmprAlgo = ALGO_INTERP;
         cmpSize = SZ_compress_Interp<T, N>(conf, data, cmpData, cmpCap);
+        std::cout<<"p5"<<std::endl;
     } else {
         // further tune lorenzo
         if (N == 3) {
