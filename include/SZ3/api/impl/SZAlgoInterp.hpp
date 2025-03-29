618f10871c3a95ad86b3023fbbc9155f1898c51a
Jliu-1 <jliu217@central.uh.edu>
2025-03-29 16:20:42 -0500

    lz

--- include/SZ3/api/impl/SZAlgoInterp.hpp
+++ include/SZ3/api/impl/SZAlgoInterp.hpp
@@ -313,7 +313,7 @@ size_t SZ_compress_Interp_lorenzo(Config &conf, T *data, uchar *cmpData, size_t 
 
         }
     }
-    bool useInterp = !(best_lorenzo_ratio > best_interp_ratio && best_lorenzo_ratio < 80 && best_interp_ratio < 80);
+    bool useInterp = !(best_lorenzo_ratio > best_interp_ratio && best_lorenzo_ratio < 40 && best_interp_ratio < 40);
     //std::cout<<best_lorenzo_ratio<<" "<<best_interp_ratio<<std::endl;
     size_t cmpSize = 0;
     if (useInterp) {
