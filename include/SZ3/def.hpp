da3d12336ab9bcff52da318643fb0ee3ab3156e6
Kai Zhao <zhaokaijia@gmail.com>
2025-06-06 15:58:48 -0400

    update inline

--- include/SZ3/def.hpp
+++ include/SZ3/def.hpp
@@ -11,8 +11,10 @@ typedef unsigned char uchar;
 
 #ifdef _MSC_VER
 #define ALWAYS_INLINE __forceinline
+#elif defined(__GNUC__) || defined(__clang__)
+#define ALWAYS_INLINE inline __attribute__((always_inline))
 #else
-#define ALWAYS_INLINE __attribute__((always_inline))
+#define ALWAYS_INLINE inline
 #endif
 
 #endif
